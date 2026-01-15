1e11f0fd347c112abb77821b0bff36c21f52e12e
Loic Mulder <loic.mulder@gmail.com>
2026-01-15 16:29:45 +0000

    Updates 2026

--- python-producer/app.py
+++ python-producer/app.py
@@ -2,21 +2,19 @@ from kafka import KafkaProducer
 import json
 
 # Define the Kafka broker and topic
-broker = '{RPI_IP}:9092'
+broker = 'kafka.loic-mulder-dev.svc.cluster.local:9092'
 topic = 'my-first-topic'
 
 # Create a Kafka producer
-#producer = KafkaProducer(
-#    bootstrap_servers=[broker],
-#   sasl_mechanism='SCRAM-SHA-256',
-#     security_protocol='SASL_PLAINTEXT',
-#     sasl_plain_username='user1',
-#     sasl_plain_password='FHp2sfyGox'
-#)
 producer = KafkaProducer(
     bootstrap_servers=[broker],
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+   sasl_mechanism='SCRAM-SHA-256',
+     security_protocol='SASL_PLAINTEXT',
+     sasl_plain_username='user1',
+     sasl_plain_password='VkEc1DVF03',
+     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
+
 # Define the message to send
 message = {
     'key': 'this-is-a-key',
