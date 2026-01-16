a17309b31a9493db54d22d7d028312ddf40f9ef4
Loic Mulder <loic.mulder@gmail.com>
2026-01-16 12:52:48 +0000

    Setup pour kube hosted kafka

--- python-producer/app.py
+++ python-producer/app.py
@@ -15,14 +15,14 @@ producer = KafkaProducer(
      value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-# Define the message to send
+# Define the message to send in a specific topic
 message = {
     'key': 'this-is-a-key',
     'value': 'this-is-a-value'
 }
 
 # Send the message to the Kafka topic
-producer.send(topic, value=message)
+producer.send(topic, partition=0, value=message)
 
 # Ensure all messages are sent before closing the producer
 producer.flush()
